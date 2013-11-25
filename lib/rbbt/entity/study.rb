c869b2690b533dbdeb68f33018e81401ac1debfe
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-11-25 13:14:26 +0100

    nil

--- lib/rbbt/entity/study.rb
+++ lib/rbbt/entity/study.rb
@@ -105,6 +105,23 @@ module Study
       select{|f| File.directory? f}.sort.collect{|s| Study.setup(File.basename(s))}
   end
 
+  def self.studies
+    case study_dir
+    when nil
+      []
+    when Path 
+      study_dir.find_all.collect do |study_path| 
+        study_path.glob('*').select{|f| f.directory? }
+      end.flatten.collect{|f| self.annotate f} 
+    else
+      Dir.glob(File.join(study_dir, "*"))
+    end.sort.collect do |dir| 
+      study = Study.setup(File.basename(dir))
+      study.dir = study_dir.annotate(dir)
+      study
+    end
+  end
+
   def dir
     @dir ||= if Path === Study.study_dir
                Study.study_dir[self] 
